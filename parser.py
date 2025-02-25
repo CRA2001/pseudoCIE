51dd035cfe5525f3706e7d2b3790dedb68c545a5
CRA2001 <carlosarro182@gmail.com>
2025-02-25 23:28:41 +0300

    fixed the recursive call error

--- parser.py
+++ parser.py
@@ -4,52 +4,57 @@ from lexer.py's tokenize() method (from the lexer class), and applies
 recursive descent on to it to construct the AST STRUCTURE
 '''
 
-import lexer
+import lexer #importing from lexer.py to take in the array of tokens
 
 class Parser:
 	#constructor for parser which takes in the tokens and the current position
 	def __init__(self,tokens):
 		self.tokens = tokens
-		self.pos = 0	
+		self.pos = 0
 	def current_token(self):
 		if self.pos < len(self.tokens):
 			return self.tokens[self.pos]
 		return None
 
 
-	def eat(self,token_type):
-
-		if self.current_token() and self.current_token().type() == token_type:
+	def moveToNext(self,token_type):
+		if self.current_token() and self.current_token()[0] == token_type:
 			self.pos+=1
 		else:
 			raise SyntaxError(f"Expected token type: {token_type}, got {self.current_token()} instead")
 	def parse(self):
-		#calling the expression() recursive funciton to control the call()
+		#calling the expression() recursive function to control the call()
 		return self.expression()
 	def expression(self):
 		node = self.term()
-		while self.current_token() and self.current_token().type in ('PLUS','MINUS'):
+		while self.current_token() and self.current_token()[0] in ('PLUS','MINUS'):
 			token = self.current_token()
-			self.eat(token.type)
-			node = ('binary_op',token.type,node,self.factor())	
+			self.moveToNext(token[0])
+			node = ('binary_op',token[0],node,self.term())
 		return node
 	def term(self):
 		node = self.factor()
-		while self.current_token() and self.current_token().type in ('MULTIPLY','DIVIDE'):
+		while self.current_token() and self.current_token()[0] in ('MULTIPLY','DIVIDE'):
 			token = self.current_token()
-			self.eat(token.type)
-			node = ('binary_op',token.type,node,self.factor())	
+			self.moveToNext(token[0])
+			node = ('binary_op',token[0],node,self.term())
 		return node
+
 	def factor(self):
 		token = self.current_token()
-		if token.type == 'NUMBER':
-			self.eat('NUMBER')
-			return ('number',token.value)
-		elif token.type == 'LPAREN':
-			self.eat('LPAREN')
-			node = self.expression()
+		if token[0] == 'NUMBER':  # Check if it's a number
+			self.moveToNext('NUMBER')  # Consume the number token
+			return ('number', token[1])  # Return the node representing the number
+		elif token[0] == 'LPAREN':  # If it's a left parenthesis
+			print("LPAREN FOUND")
+			self.moveToNext('LPAREN')  # Consume the LPAREN token
+			node = self.expression()  # Parse the expression inside the parentheses
+			self.moveToNext('RPAREN')  # Consume the closing RPAREN
+			return ('paren', node)  # Return the node wrapped inside 'paren'
 		else:
 			raise SyntaxError(f'Unexpected token: {token}')
+
+
 #test value
 if __name__ == "__main__":
 	code =  "3+5 * (2-1)"
@@ -59,13 +64,6 @@ if __name__ == "__main__":
 	parser = Parser(tokens)
 	ast = parser.parse()
 	print("Parsed AST: ",ast)
-
-
-
-
-
-
-
 
 
 
